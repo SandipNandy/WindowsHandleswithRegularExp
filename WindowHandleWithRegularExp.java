7df310204bb5eb6146b4c76a5ca887623e3d0323
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-15 20:26:42 +0530

    2nd commit
    directory is changed

--- WindowHandleWithRegularExp.java
+++ WindowHandleWithRegularExp.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WindowHandleWithRegularExp {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", ".\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
